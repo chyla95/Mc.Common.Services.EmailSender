378d90927809082a1e5ba0a3cd0dcabfee7f68d0
chyla95 <chyla95@gmail.com>
2024-08-24 02:31:36 +0200

    Remove tests

--- tests/Mc.Common.Services.EmailSender.DevelopmentTests/DependencyInjection/DependencyInjectionBuilderOptions.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Shing.Common.Architecture.Mailing.DevelopmentTests.DependencyInjection;
-
-internal sealed class DependencyInjectionBuilderOptions
-{
-    public string? EnvironmentName { get; set; }
-}
--- tests/Mc.Common.Services.EmailSender.DevelopmentTests/DependencyInjection/DependencyInjectionConstants.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Shing.Common.Architecture.Mailing.DevelopmentTests.DependencyInjection;
-
-internal static class DependencyInjectionConstants
-{
-    public static class Environment
-    {
-        public const string EnvironmentName = "DOTNET_ENVIRONMENT";
-    }
-}
